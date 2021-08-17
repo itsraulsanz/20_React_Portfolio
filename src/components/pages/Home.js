4c26f058c3a3e3f1e8d0838a761f355f30d75817
Raul Sanz <luar_znas@hotmail.com>
2021-08-17 20:18:19 +0100

    Updated data

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,27 +1,24 @@
 import React from "react";
 
 export default function Home() {
-  var profileImage = '../images/profile_img.jpg';
+  var profileImage = "../images/profile_img.jpg";
 
   return (
     <section>
       <h2>About me</h2>
       <div className="about_info_container">
-        <img
-          alt="Raul_picture"
-          className="profile_img"
-          src={profileImage}
-        />
+        <img alt="Raul_picture" className="profile_img" src={profileImage} />
         <p>
-          Creative and passionate designer with an established Digital Design
-          experience alongside a vast amount of knowledge within marketing and
-          advertising. Including a multiple skill set that compliments the
-          disciplines of Design, programming and UX. Currently I am studying Full
-          Stack Developer at University of Manchester. 
-        </p>  
+          Front end web developer with a background in digital design ready to
+          provide my experience in UX, marketing and advertising. After
+          completing the accredited University of Manchester Coding Boot Camp, I
+          am looking for a challenging position where I can focus and improve
+          upon my passion for coding.
+        </p>
         <p>
-          Moving forward I am looking for a challenging position as a developer, in which my passion for digital industry and experience will be valued.
-        </p>      
+          My strengths are creativity, teamwork, and building projects from
+          ideation to execution.
+        </p>
       </div>
     </section>
   );
