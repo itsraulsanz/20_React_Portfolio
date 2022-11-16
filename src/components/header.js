0c3905932a3ae0fd2974126be9cf7135e39a999a
Raul Sanz <luar_znas@hotmail.com>
2022-11-16 18:44:52 +0000

    Change header into a component

--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,11 @@ import React from "react";
 var logo = "../images/logo.svg";
 
 export default function Header() {
-  
   return (
     <div>
-      <div>
+      <a className="logo" href="#about-me">
         <img alt="logo" className="logo" src={logo} />
-      </div>
+      </a>
     </div>
   );
 }
