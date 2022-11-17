657b81b35a3a1c34514ea27c18136a7271d4114d
Raul Sanz <luar_znas@hotmail.com>
2022-11-17 17:46:23 +0000

    Move header and footer into component folders

--- src/PortfolioContainer.js
+++ src/PortfolioContainer.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Navigation from './components/navigation';
-import Header from './components/header';
-import Footer from './components/footer';
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
 import Home from './pages/Home';
 
 export default function PortfolioContainer() {
   return (
     <div className='body-container'>
     <header>
-      <Header />
-      <Navigation /> 
+      <div className='header_container'>
+        <Header />
+        <Navigation /> 
+      </div>  
     </header>
     <main>
       <Home />
