0388bacf5b42fcb84decd9130b06addadb4454b8
Raul Sanz <luar_znas@hotmail.com>
2022-11-21 22:21:03 +0000

    Update index root

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import './css/style.scss'
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
